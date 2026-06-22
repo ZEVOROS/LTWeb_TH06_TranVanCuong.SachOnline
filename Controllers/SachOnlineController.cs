a9e2ab81dc2260395b4710824b0bdc468a33a209
ZEVOROS <vancuong140404@gmail.com>
2026-06-22 10:33:11 +0700

    Add project files.

---
+++ Controllers/SachOnlineController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TranVanCuong.SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        SachOnlineEntities data = new SachOnlineEntities();
+
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACH
+                       .OrderByDescending(s => s.NgayCapNhat)
+                       .Take(count)
+                       .ToList();
+        }
+
+        public ActionResult Index()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDE
+                            select cd;
+
+            return PartialView(listChuDe);
+        }
+
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNXB = data.NHAXUATBAN.ToList();
+            return PartialView(listNXB);
+        }
+
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listSach = data.SACH
+                               .OrderByDescending(s => s.SoLuongBan)
+                               .Take(6)
+                               .ToList();
+
+            return PartialView(listSach);
+        }
+
+        public ActionResult BookDetail(int id)
+        {
+            var sach = data.SACH.SingleOrDefault(s => s.MaSach == id);
+            return View(sach);
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TranVanCuong.SachOnline")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TranVanCuong.SachOnline")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ac0579a-7ef3-4549-a460-3644342a06bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
